6a14d4b803aa7547f67bb83ff888fc392260f3fb
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-02-17 14:46:35 +0100

    i dont even know what

---
+++ Glitch Gardeen/Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float speedOfSpin = -180f;
+
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
