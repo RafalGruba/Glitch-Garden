6551e81938fc099f96e1e3d3f61dfa172a49dbf7
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-02-17 20:58:20 +0100

    Big update
    Gierka jest aktualnie na etapie gdzie jaszczury atakują z prawej, a kaktusy strzelają do nich z cukiń, które biją jaszczury na hita.

--- Glitch Gardeen/Assets/Scripts/Projectile.cs
+++ Glitch Gardeen/Assets/Scripts/Projectile.cs
@@ -6,10 +6,24 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
     [SerializeField] float speedOfSpin = -180f;
+    [SerializeField] float damage = 50f;
+
 
     void Update()
     {
         transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
         transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
 }
