cd141a370e0f5cd03c400ff87d00a70595de9c66
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-02-18 12:54:49 +0100

    more things
    -added StarDisplay.cs with UpdateDisplay(), AddStars() and SpendStarts()
    -added TMPro under Canvas and assigned to it StarDisplay

---
+++ Glitch Gardeen/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    TMP_Text starText;
+
+
+    void Start()
+    {
+        starText = GetComponent<TMP_Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+    }
+
+
+
+
+}
