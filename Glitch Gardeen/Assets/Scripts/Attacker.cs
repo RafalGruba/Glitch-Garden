fd943402c13efa253d3a02764379d4e9ce1c7f0e
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-04-09 17:34:19 +0200

    Make Attackers Attack

--- Glitch Gardeen/Assets/Scripts/Attacker.cs
+++ Glitch Gardeen/Assets/Scripts/Attacker.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range (0f, 5f)] float currentSpeed = 1f;
+    GameObject currentTarger;
 
 
     // Start is called before the first frame update
@@ -22,5 +23,11 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarger = target;
     }
 }
