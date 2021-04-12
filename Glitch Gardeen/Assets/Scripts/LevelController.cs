dbb1e1cffedbb5dc218cef1a7ea32bc16e93d0f9
RafalGruba <66263211+RafalGruba@users.noreply.github.com>
2021-04-12 19:08:05 +0200

    Level Controller creation

---
+++ Glitch Gardeen/Assets/Scripts/LevelController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    int NumberOfAttackers = 0;
+    bool levelTimerFinished = false;
+
+    public void AttackerSpawned()
+    {
+        NumberOfAttackers++;
+    }
+    public void AttackerKilled()
+    {
+        NumberOfAttackers--;
+        if (NumberOfAttackers <= 0 && levelTimerFinished)
+        {
+            Debug.Log("End Level Now!");
+        }
+    }
+
+    public void LevelTimerFinished()
+    {
+        levelTimerFinished = true;
+        StopSpawners();
+    }
+
+    private void StopSpawners()
+    {
+        AttackerSpawner[] spawnerArray = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawnerArray)
+        {
+            spawner.StopSpawning();
+        }
+    }
+}
